15e51ed56e37f4b3cbac9e592553586dfa3bf82d
DougyShy <90754920+DougyShy@users.noreply.github.com>
2025-02-27 17:02:38 -0600

    working

--- messaging-app-backend/server.js
+++ messaging-app-backend/server.js
@@ -11,6 +11,14 @@ const port = process.env.PORT || 9000;
 const connection_url =
   "mongodb+srv://dougscheible:fossil69@cluster1.2l0ue.mongodb.net/messagingDB?retryWrites=true&w=majority";
 
+const pusher = new Pusher({
+  appId: "1948552",
+  key: "5ae506e78bee62c643ca",
+  secret: "6a2af2bebf2463d102d0",
+  cluster: "us2",
+  useTLS: true,
+});
+
 //Middleware
 app.use(express.json());
 app.use(Cors());
@@ -19,6 +27,27 @@ app.use(Cors());
 mongoose.connect(connection_url, {});
 
 //API Endpoints
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("DB connected");
+  const msgCollection = db.collection("messagingmessages");
+  const changeStream = msgCollection.watch();
+  changeStream.on("change", (change) => {
+    console.log(change);
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.name,
+        message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
+      });
+    } else {
+      console.log("Error trigerring Pusher");
+    }
+  });
+});
+
 app.get("/", (req, res) => res.status(200).send("Hello TheWebDev"));
 
 app.get("/messages/sync", async (req, res) => {
